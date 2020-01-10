012a4612805d8c0cf743f34ca4fbad92495a6cc3
Pooja Madan <pmadan@pmadan-macos.corp.adobe.com>
2020-01-10 17:53:57 +0530

    first commit

---
+++ ABCD.py
@@ -0,0 +1,14 @@
+for x in range(65,91):
+	print(chr(x),end='  ')
+else:
+	print()
+#For printing caps ABCD 
+
+for y in range(97,123):
+	print(chr(y),end='  ')
+else:
+	print()
+	print()
+# last print is to get the cursor in next line after everything is done
+# For Printing small abcd..z
+
